b055c484c55a1f99ea7d8837c23b22aa206fd9c2
jdmunta <jagadesh.munta@gmail.com>
2023-12-15 19:20:37 -0800

    adding models

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -75,9 +75,17 @@ WSGI_APPLICATION = "littlelemon.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
+    "sqllitedb": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
+    },
+    "default": {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'littlelemon_restaurant',
+        'HOST' : '127.0.0.1',
+        'PORT' : '3306',
+        'USER' : 'root',
+        'PASSWORD' : 'root321!',
     }
 }
 
