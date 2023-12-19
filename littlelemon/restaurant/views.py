18da9aa0907d5785807cf5e7fa5d0e7bc9ee2631
jdmunta <jagadesh.munta@gmail.com>
2023-12-18 20:42:18 -0800

    adding through tables

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import MenuSerializer, BookingSerializer
+from .models import Menu, Booking
+from rest_framework import viewsets
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+    class Meta:
+        model = Booking
+        serializer_class = BookingSerializer
